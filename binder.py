a3f28a78832ca0a066bbffb5fcd2e69d7ca29e0e
Jared Kotoff <krylic@gmail.com>
2015-11-20 18:59:12 -0800

    Formating

--- binder.py
+++ binder.py
@@ -3,8 +3,9 @@ import sys
 from subprocess import call
 from subprocess import Popen, PIPE
 
-# The file name 
-FILE_NAME = "codearray.h";
+# The file name
+FILE_NAME = "codearray.h"
+
 
 ###########################################################
 # Returns the hexidecimal dump of a particular binary file
@@ -15,24 +16,24 @@ FILE_NAME = "codearray.h";
 # For example, 0x19,0x12,0x45,0xda,
 ##########################################################
 def getHexDump(execPath):
-	
-	# The return value
-	retVal = None
-	
-	# TODO:
-	# 1. Use popen() in order to run hexdump and grab the hexadecimal bytes of the program.
-	# 2. If hexdump ran successfully, return the string retrieved. Otherwise, return None.
-	# The command for hexdump to return the list of bytes in the program in C++ byte format
-	# the command is hexdump -v -e '"0x" 1/1 "%02X" ","' progName
 
-	p = Popen(["hexdump", "-v", "-e", "\"0x\" 1/1 \"%02x\" \", \"", execPath], stdout=PIPE)
+    # The return value
+    retVal = None
 
-	(out, err) = p.communicate()
+    # DONE:
+    # 1. Use popen() in order to run hexdump and grab the hexadecimal bytes of the program.
+    # 2. If hexdump ran successfully, return the string retrieved. Otherwise, return None.
+    # The command for hexdump to return the list of bytes in the program in C++ byte format
+    # the command is hexdump -v -e '"0x" 1/1 "%02X" ","' progName
 
-	returnCode = p.wait()
+    p = Popen(["hexdump", "-v", "-e", "\"0x\" 1/1 \"%02x\" \", \"", execPath], stdout=PIPE)
 
-	if returnCode == 0:
-		retVal = out
+    (out, err) = p.communicate()
+
+    returnCode = p.wait()
+
+    if returnCode == 0:
+        retVal = out
 
 	return retVal
 
@@ -46,56 +47,53 @@ def getHexDump(execPath):
 def generateHeaderFile(execList, fileName):
 
 	# The header file
-	headerFile = None
+    headerFile = None
 
-	# The program array
-	progNames = sys.argv
+    # The program array
+    progNames = sys.argv
 
-	# Open the header file
-	headerFile = open(fileName, "w")
+    # Open the header file
+    headerFile = open(fileName, "w")
 
-	# The program index
-	progCount = 0
+    # The program index
+    progCount = 0
 
-	# The lengths of programs
-	progLens = []
+    # The lengths of programs
+    progLens = []
 
-	# Write the array name to the header file
-	headerFile.write("#include <string>\n\nusing namespace std;\n\nunsigned char* codeArray[] = {");
+    # Write the array name to the header file
+    headerFile.write("#include <string>\n\nusing namespace std;\n\nunsigned char* codeArray[] = {");
 
-	
-	
-	# TODO: for each program progName we should run getHexDump() and get the 
-	# the string of bytes formatted according to C++ conventions. That is, each
-	# byte of the program will be a two-digit hexadecimal value prefixed with 0x. 
-	# For example, 0xab. Each such byte should be added to the array codeArray in 
-	# the C++ header file. After this loop executes, the header file should contain 
-	# an array of the following format:
-	# 1. unsigned char* codeArray[] = {new char[<number of bytes in prog1>{prog1byte1, prog1byte2.....},
-	# 				   new char[<number of bytes in prog2><{prog2byte1, progbyte2,....},
-	#					........
-	#				};
+    # TODO: for each program progName we should run getHexDump() and get the
+    # the string of bytes formatted according to C++ conventions. That is, each
+    # byte of the program will be a two-digit hexadecimal value prefixed with 0x.
+    # For example, 0xab. Each such byte should be added to the array codeArray in
+    # the C++ header file. After this loop executes, the header file should contain
+    # an array of the following format:
+    # 1. unsigned char* codeArray[] = {new char[<number of bytes in prog1>{prog1byte1, prog1byte2.....},
+    # 				   new char[<number of bytes in prog2><{prog2byte1, progbyte2,....},
+    # 					........
+    # 				};
 
 
-	
-	# Add array to containing program lengths to the header file
+
+    # Add array to containing program lengths to the header file
         headerFile.write("\n\nunsigned programLengths[] = {")
 
         # The number of programs
-        numProgs = 0 
-	
-	# TODO: add to the array in the header file the sizes of each program.
-	# That is the first element is the size of program 1, the second element
-	# is the size of program 2, etc.
-	for progName in execList:
-		pass
+        numProgs = 0
 
-	
-	# TODO: Write the number of programs.
-	headerFile.write("\n\n#define NUM_BINARIES " +  str(len(progNames) - 1))
-	
-	# Close the header file
-	headerFile.close()
+    # TODO: add to the array in the header file the sizes of each program.
+    # That is the first element is the size of program 1, the second element
+    # is the size of program 2, etc.
+    for progName in execList:
+        pass
+
+        # TODO: Write the number of programs.
+        headerFile.write("\n\n#define NUM_BINARIES " + str(len(progNames) - 1))
+
+        # Close the header file
+        headerFile.close()
 
 
 ############################################################
@@ -104,15 +102,15 @@ def generateHeaderFile(execList, fileName):
 # @param execName - the executable file name
 ############################################################
 def compileFile(binderCppFileName, execName):
-	
-	print("Compiling...")
-	
-	# Run the process
-	# TODO: run the g++ compiler in order to compile backbinder.cpp
-	# If the compilation succeeds, print "Compilation succeeded"
-	# If compilation failed, then print "Compilation failed"	
-	# Do not forget to add -std=gnu++11 flag to your compilation line
-	pass
 
-generateHeaderFile(sys.argv[1:], FILE_NAME)	
+    print("Compiling...")
+
+    # Run the process
+    # TODO: run the g++ compiler in order to compile backbinder.cpp
+    # If the compilation succeeds, print "Compilation succeeded"
+    # If compilation failed, then print "Compilation failed"
+    # Do not forget to add -std=gnu++11 flag to your compilation line
+    pass
+
+generateHeaderFile(sys.argv[1:], FILE_NAME)
 compileFile("binderbackend.cpp", "bound")
